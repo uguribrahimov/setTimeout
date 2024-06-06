1bdb7e0fc7468a817032992d29b687d3960918cd
uguribrahimov <uguribrahimov04@gmail.com>
2024-06-06 21:10:39 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+setTimeout(function() {
+    console.log("Hello world");
+  }, 0);
+
+
+
+
+
+
+
+// var counter = 0;
+// var intervalId = setInterval(function() {
+//     counter++;
+//     console.log("Counter:", counter);
+//     if (counter === 10) {
+//         clearInterval(intervalId);
+//         console.log("Interval dayandırıldı!");
+//     }
+// }, 5000);
+
